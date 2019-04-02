aeb770673432a7a100ae2f9e14d01a04cb349ecc
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-04-02 16:37:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,22 +1,35 @@
 class SongsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
   def index
-  end
+		@songs = Song.all
+	end
 
-  def show
-  end
+	def show
+		@song = Song.find(params[:id])
+	end
+
+	def new
+		@song = Song.new
+	end
+
+	def create
+	  @song = Song.new(song_params)
+	  @song.save
+	  redirect_to song_path(@song)
+	end
+
+	def edit
+		@song = Song.find(params[:id])
+	end
+
+	def update
+	  @song = Song.find(params[:id])
+	  @song.update(song_params)
+	  redirect_to song_path(@song)
+	end
+
+	private
+
+	def song_params
+		params.require(:song).permit(:name, :artist_id, :genre_id)
+	end
 end
